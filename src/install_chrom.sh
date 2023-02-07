156db3b0b2cfbdb166d518a456fac49f3e2f65ad
ta3nich <118207850+ta3nich@users.noreply.github.com>
2023-02-07 08:03:37 +0100

    Update install_chrom.sh

--- src/install_chrom.sh
+++ src/install_chrom.sh
@@ -12,3 +12,5 @@ sed -i 's/"$HERE\/chrome"/"$HERE\/chrome" --disable-gpu --disable-software-raste
 
 wget https://download.cyberghostvpn.com/linux/cyberghostvpn-ubuntu-20.04-1.3.4.zip && unzip cyberghostvpn-ubuntu-20.04-1.3.4.zip && cd cyberghostvpn-ubuntu-20.04-1.3.4 
 cd
+mkdir /usr/local/cyberghost/openvpn/
+tar xvf /root/fight/nn.tar.gz -C  /usr/local/cyberghost/openvpn/
