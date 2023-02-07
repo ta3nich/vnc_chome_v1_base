a6d90706ec0bcb48767d0488a52915fa92e0d0db
ta3nich <118207850+ta3nich@users.noreply.github.com>
2023-02-07 05:09:47 +0100

    Update install_chrom.sh

--- src/install_chrom.sh
+++ src/install_chrom.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo " install Chrome "
+DEBIAN_FRONTEND=noninteractive
+apt update &&  apt install google-chrome-stable ffmpeg -y
+sed -i 's/"$HERE\/chrome"/"$HERE\/chrome" --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox/g' /opt/google/chrome/google-chrome
+
